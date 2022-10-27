dca0d88588be9f1165ba0eb2b1f49c3e04364c83
JustSteve2k <srodriguez2k@gmail.com>
2022-10-27 15:40:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Product from "./components/product";
+import "./App.css";
 
 function App() {
+  const DUMMY_PRODUCT_LIST = [
+    { id: 1, item: "shirt" },
+    { id: 2, item: "pants" },
+    { id: 3, item: "shorts" },
+    { id: 4, item: "hat" },
+    { id: 5, item: "socks" },
+    { id: 6, item: "shoes" },
+  ];
+
+  // const [products, setProducts] = useState(DUMMY_PRODUCT_LIST);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container bg-slate-700  ">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </div>
   );
 }
