5c594d41f0403667a96c35076dfc0f2f65cc663e
JustSteve2k <srodriguez2k@gmail.com>
2022-10-28 22:01:58 -0500

    setup base cart component

---
+++ src/components/cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./cart.css";
+
+export default function Cart() {
+  return (
+    <div className="cartContainer">
+      <h1>Cart</h1>
+      <div>Stuff in your cart</div>
+      <h2>Cart is empty atm</h2>
+    </div>
+  );
+}
