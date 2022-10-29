46c7aae94126a7c3d08a24fc78acac434779d12b
JustSteve2k <srodriguez2k@gmail.com>
2022-10-28 22:02:19 -0500

    dynamiclly setup product tiles

--- src/components/product.js
+++ src/components/product.js
@@ -2,6 +2,11 @@ import React from "react";
 
 import "./product.css";
 
-export default function Product() {
-  return <div className="product">product</div>;
+export default function Product(props) {
+  return (
+    <div className="product">
+      {props.item}
+      <button>Add to Cart</button>
+    </div>
+  );
 }
