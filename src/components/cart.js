a02c87f52de613d46a465d20757b4c3a68af1e45
JustSteve2k <srodriguez2k@gmail.com>
2022-10-29 22:55:43 -0500

    add cart functionality

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./cart.css";
 
-export default function Cart() {
+export default function Cart(props) {
+  const clickHandler = () => {
+    console.log(props.item);
+  };
+
+  const list = () => {
+    <ul>
+      {props.item.map((element) => (
+        <li>element.item</li>
+      ))}
+    </ul>;
+  };
+
   return (
     <div className="cartContainer">
       <h1>Cart</h1>
       <div>Stuff in your cart</div>
-      <h2>Cart is empty atm</h2>
+      <button onClick={clickHandler}>See whats in the cart</button>
+      {list}
     </div>
   );
 }
--- src/components/product.js
+++ src/components/product.js
@@ -3,10 +3,17 @@ import React from "react";
 import "./product.css";
 
 export default function Product(props) {
+  const clickHandler = () => {
+    let item = props.item;
+    let id = props.id;
+
+    props.cartHandler(item, id);
+  };
+
   return (
     <div className="product">
       {props.item}
-      <button>Add to Cart</button>
+      <button onClick={clickHandler}>Add to Cart</button>
     </div>
   );
 }
