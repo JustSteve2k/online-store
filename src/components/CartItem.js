13dd6c02389a2da64acc990b56904f9e2707d340
JustSteve2k <srodriguez2k@gmail.com>
2022-10-30 23:57:08 -0500

    get cart to show added items.

---
+++ src/components/CartItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CartItem(props) {
+  if (props.item.length === 0) {
+    return <li>Nothing in here yet</li>;
+  }
+
+  return <li>{props.item}</li>;
+}
--- src/components/cart.js
+++ src/components/cart.js
@@ -1,25 +1,33 @@
 import React from "react";
 import "./cart.css";
+import "./CartItem";
+import CartItem from "./CartItem";
 
 export default function Cart(props) {
   const clickHandler = () => {
     console.log(props.item);
   };
 
-  const list = () => {
+  const list = (
     <ul>
       {props.item.map((element) => (
         <li>element.item</li>
       ))}
-    </ul>;
-  };
+    </ul>
+  );
+
+  const test = <p>TEST HELLO COMPONENT</p>;
 
   return (
     <div className="cartContainer">
       <h1>Cart</h1>
       <div>Stuff in your cart</div>
       <button onClick={clickHandler}>See whats in the cart</button>
-      {list}
+      <ul>
+        {props.item.map((element) => (
+          <CartItem key={element.id} item={element.item} />
+        ))}
+      </ul>
     </div>
   );
 }
