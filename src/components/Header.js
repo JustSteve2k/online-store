6a6a1a8b3897abeb101457998a7922855aeac855
JustSteve2k <srodriguez2k@gmail.com>
2022-11-29 23:08:36 -0600

    updated header conditionals and more context

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,18 @@ import "./Header.css";
 import { LoggedInContext } from "../context/LoggedInContext";
 
 export default function Header(props) {
-  const test = useContext(LoggedInContext);
+  // const test = useContext(LoggedInContext);
+  const { test, alttest, isAdmin, isLoggedIn } = useContext(LoggedInContext);
 
   return (
     <div className="headerMain">
       Logo!
       {test}
+      {alttest}
       <div className="links">
-        <button>Admin</button>
-        <button>Account</button>
-        <button onClick={props.showCartHandler}>Cart</button>
+        {isAdmin && <button>Admin</button>}
+        {isLoggedIn ? <button>Account</button> : <button>Login</button>}
+        <button onClick={props.displayCartHandler}>Cart</button>
       </div>
     </div>
   );
--- src/context/LoggedInContext.js
+++ src/context/LoggedInContext.js
@@ -4,6 +4,9 @@ export const LoggedInContext = React.createContext();
 
 export default function LoggedInProvider({ children }) {
   const test = "works!";
+  const alttest = "alsoworks!";
+  const isAdmin = true;
+  const isLoggedIn = false;
 
-  return <LoggedInContext.Provider value={test}>{children}</LoggedInContext.Provider>;
+  return <LoggedInContext.Provider value={{ test, alttest, isAdmin, isLoggedIn }}>{children}</LoggedInContext.Provider>;
 }
