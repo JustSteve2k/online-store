46272afaf5d4d5b88745e5d20ab15d13ffaaa4e8
JustSteve2k <srodriguez2k@gmail.com>
2022-12-01 15:44:08 -0600

    show user and admin panel access

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { LoggedInContext } from "../context/LoggedInContext";
 
 export default function Header(props) {
   // const test = useContext(LoggedInContext);
-  const { test, alttest, isAdmin, isLoggedIn } = useContext(LoggedInContext);
+  const { test, alttest, currentUser, isAdmin, isLoggedIn, logOutHandler } = useContext(LoggedInContext);
 
   return (
     <div className="headerMain">
@@ -13,9 +13,10 @@ export default function Header(props) {
       {test}
       {alttest}
       <div className="links">
-        {isAdmin && <button>Admin</button>}
-        {isLoggedIn ? <button>Account</button> : <button onClick={props.showLoginHandler}>Login</button>}
+        {isAdmin && <button>AdminPanel</button>}
+        {isLoggedIn ? <button>{currentUser}</button> : <button onClick={props.showLoginHandler}>Login</button>}
         <button onClick={props.displayCartHandler}>Cart</button>
+        {isLoggedIn && <button onClick={logOutHandler}>LogOut</button>}
       </div>
     </div>
   );
