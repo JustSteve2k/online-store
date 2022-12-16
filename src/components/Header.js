a2af7f10727de7a664f8aa030662ec1a413ea718
JustSteve2k <srodriguez2k@gmail.com>
2022-12-16 14:29:50 -0600

    remove extra code

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,13 @@ import React, { useContext } from "react";
 import "./Header.css";
 
 import { LoggedInContext } from "../context/LoggedInContext";
-// import { ProductContext } from "../context/ProductContext";
-// import { TestContext } from "../context/TestContext";
 
 export default function Header(props) {
-  // const { contextTest, currentUser, isAdmin, isLoggedIn, logOutHandler } = useContext(LoggedInContext);
   const { currentUser, isAdmin, isLoggedIn, logOutHandler } = useContext(LoggedInContext);
-  const logCTX = useContext(LoggedInContext);
-  // const productCTX = useContext(ProductContext);
 
   return (
     <div className="headerMain">
       Logo!
-      {logCTX.contextTest}
       <div className="links">
         {isAdmin && <button>AdminPanel</button>}
         {isLoggedIn ? <button>{currentUser}</button> : <button onClick={props.showLoginHandler}>Login</button>}
