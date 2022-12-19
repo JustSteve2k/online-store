ee4ff474f276e430256d30e98d71d92c0123ed7f
JustSteve2k <srodriguez2k@gmail.com>
2022-12-19 15:58:53 -0600

    fix links on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,8 @@
 import React, { useContext, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import Modal from "./Modal/Modal";
+import Login from "./Login/Login";
 
 import "./Header.css";
 
@@ -31,18 +35,23 @@ export default function Header(props) {
   }, [items]);
 
   // const cartCount = totalItems > 0 ? <span> - {totalItems}</span> : "";
-  const cartCount = <> Cart {totalItems > 0 ? <span> - {totalItems}</span> : ""} </>;
+  const cartCount = <>Cart {totalItems > 0 ? <span> - {totalItems}</span> : ""}</>;
 
   return (
     <div className="headerMain">
-      <img src={logo} alt="logo" />
+      <NavLink to="/store">
+        <img src={logo} alt="logo" />
+      </NavLink>
       <div className="links">
+        <NavLink to="/about">About</NavLink>
         {isAdmin && <button>AdminPanel</button>}
         {isLoggedIn ? <button>{currentUser}</button> : <button onClick={props.showLoginHandler}>Login</button>}
         <button className={cartBtnClasses} onClick={props.showCartHandler}>
           {cartCount}
         </button>
         {isLoggedIn && <button onClick={logOutHandler}>LogOut</button>}
+        {/* {props.showCart && <Modal showCartHandler={props.showCartHandler} />}
+        {props.showLogin && <Login showLoginHandler={props.showLoginHandler} />} */}
       </div>
     </div>
   );
