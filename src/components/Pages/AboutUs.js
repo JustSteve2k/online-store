c6c74130abff73eb009ab928dc4677a176e68c1c
JustSteve2k <srodriguez2k@gmail.com>
2022-12-19 15:54:57 -0600

    setup about us page

---
+++ src/components/Pages/AboutUs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from "./AboutUs.module.css";
+
+export default function AboutUs() {
+  return (
+    <div className={classes.background}>
+      <h2>About Us</h2>
+      <p className={classes.description}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem alias molestiae reiciendis a assumenda sint veritatis ea, error mollitia. Molestiae
+        porro eius ea dolores ad quia quis ipsa magni, reiciendis eligendi voluptatem accusamus? Debitis similique unde quisquam ex modi molestiae temporibus,
+        totam id quos, repellat recusandae commodi dignissimos non nostrum!
+      </p>
+    </div>
+  );
+}
