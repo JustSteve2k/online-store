387a20dfd7d748a0081fe5d20c99001c7c85cd32
JustSteve2k <srodriguez2k@gmail.com>
2022-12-18 23:58:46 -0600

    setup basic homepage

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Home.css";
+
+export default function Home() {
+  const slogan = "Come find your next purchase.";
+
+  return (
+    <div class="background">
+      <h1 class="slogan">Welcome to the online store</h1>
+    </div>
+  );
+}
