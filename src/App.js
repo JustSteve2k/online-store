d08f5ac48b568e146307bc3d6eb496420d77c505
JustSteve2k <srodriguez2k@gmail.com>
2022-12-20 20:56:06 -0600

    rework routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
-import Header from "./components/Header";
 import Store from "./components/Store/Store";
+import ProductDetails from "./components/Store/ProductDetails";
 import ComboProvider from "./context/ComboProvider";
 import AboutUs from "./components/Pages/AboutUs";
 import Modal from "./components/Modal/Modal";
 import Login from "./components/Login/Login";
+import MainLayout from "./components/Layouts/MainLayout";
 
 import "./App.css";
 
@@ -26,29 +27,29 @@ function App() {
     setShowLogin((prev) => !prev);
   };
 
-  const store = (
-    <div>
-      {" "}
-      <Header showCart={showCart} showLogin={showLogin} showCartHandler={showCartHandler} showLoginHandler={showLoginHandler} />
-      <Store showCart={showCart} showLogin={showLogin} showCartHandler={showCartHandler} showLoginHandler={showLoginHandler} />
-    </div>
-  );
+  const errorPage = <div>You found the error page dude.</div>;
 
-  const aboutUs = (
-    <div>
-      {" "}
-      <Header showCartHandler={showCartHandler} showLoginHandler={showLoginHandler} />
-      <AboutUs />
-    </div>
-  );
+  // const passIns = {
+  //   showCart: showCart,
+  //   showLogin: showLogin,
+  //   showCartHandler: showCartHandler,
+  //   showLoginHandler: showLoginHandler,
+  // };
 
   return (
     <ComboProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={aboutUs} />
-          <Route path="/store" element={store} />
+          <Route
+            path="/store"
+            element={<MainLayout showCart={showCart} showLogin={showLogin} showCartHandler={showCartHandler} showLoginHandler={showLoginHandler} />}
+          >
+            <Route index element={<Store />} />
+            <Route path="about" element={<AboutUs />} />
+            <Route path="/store/item/:productID" element={<ProductDetails />} />
+          </Route>
+          <Route path="*" element={errorPage} />
         </Routes>
       </BrowserRouter>
       {showCart && <Modal showCartHandler={showCartHandler} />}
