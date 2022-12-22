893a05fd54f6700f2ef797b2286babd28d7afa3b
JustSteve2k <srodriguez2k@gmail.com>
2022-12-22 15:43:56 -0600

    add data configuration file

---
+++ src/Data.js
@@ -0,0 +1,16 @@
+const Data = {
+    // Page titles,
+    homeTitle:"Home Screen",
+    storeTitle:"My Store",
+    aboutTitle:"About Us",
+
+    // Welcome string,
+    homeScreenGreeting:"Come and check out our stuff!",
+    
+    // Test values
+    embeddedTest:{
+        test:"it works!"
+    }
+}
+
+export default Data;
