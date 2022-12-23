a1d6510da28d20c659cab5926beb988bc10a0dee
JustSteve2k <srodriguez2k@gmail.com>
2022-12-22 21:29:09 -0600

    add base data values

--- src/Data.js
+++ src/Data.js
@@ -1,16 +1,39 @@
 const Data = {
-    // Page titles,
-    homeTitle:"Home Screen",
-    storeTitle:"My Store",
-    aboutTitle:"About Us",
+  // Page titles,
+  homeTitle: "Home Screen",
+  storeTitle: "My Store",
+  aboutTitle: "About Us",
 
-    // Welcome string,
-    homeScreenGreeting:"Come and check out our stuff!",
-    
-    // Test values
-    embeddedTest:{
-        test:"it works!"
-    }
-}
+  // Welcome string on home splash screen,
+  homeScreenGreeting: "Come and check out our stuff!",
+
+  // Debug mode setting,
+  verboseMode: false,
+
+  // Test values
+  embeddedTest: {
+    test: "it works!",
+  },
+
+  // Future items: logo used,
+};
+
+// Starter values for store.
+export const DUMMY_PRODUCT_LIST = [
+  { id: 1, item: "Shirt", cost: 19.99, categories: "top" },
+  { id: 2, item: "Pants", cost: 26.99, categories: "bottom" },
+  { id: 3, item: "Shorts", cost: 14.69, categories: "bottom" },
+  { id: 4, item: "Hat", cost: 12.25, categories: "accessory" },
+  { id: 5, item: "Socks", cost: 6.99, categories: "accesory" },
+  { id: 6, item: "Shoes", cost: 49.99, categories: "shoes" },
+  { id: 7, item: "Tie", cost: 9.99, categories: "accessory" },
+  { id: 8, item: "Suspenders", cost: 12.99, categories: "accessory" },
+  { id: 9, item: "Bowtie", cost: 4.99, categories: "accessory" },
+  { id: 10, item: "Pipe", cost: 11.99, categories: "accesory" },
+  { id: 11, item: "Jorts", cost: 18.99, categories: "bottom" },
+  { id: 12, item: "Loafers", cost: 63.99, categories: "shoes" },
+  { id: 13, item: "Puffer Jacket", cost: 18.99, categories: "top" },
+  { id: 14, item: "Fake Mustache", cost: 9.99, categories: "accessory" },
+];
 
 export default Data;
