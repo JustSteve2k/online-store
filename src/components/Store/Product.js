daf9826647fb8cb4f70b497f0001b76b568ce2e7
JustSteve2k <srodriguez2k@gmail.com>
2023-01-04 23:39:53 -0600

    convert to tw from css

--- src/components/Store/Product.js
+++ src/components/Store/Product.js
@@ -29,8 +29,8 @@ export default function Product(props) {
   const newPath = "item/" + props.element.id;
 
   return (
-    <div className="product">
-      <div className="productImage">
+    <div className="product flex flex-col justify-center bg-slate-700 w-96 h-96">
+      <div className="productImage h-60 border-black border-solid border-2">
         <img src={productImage} alt="Generic Product" />
       </div>
       <span className="productInfo mt-2 text-white ">
