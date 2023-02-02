349340f98bba94ad977b01909a52f17b7cf5aed8
JustSteve2k <srodriguez2k@gmail.com>
2023-02-02 01:33:04 -0600

    add link to edit products in admin  page

--- src/components/Pages/Admin.js
+++ src/components/Pages/Admin.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { redirect, Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 import { LoggedInContext } from "../../context/LoggedInContext";
 
@@ -7,16 +7,16 @@ export default function UserProfile() {
   const loggedInCTX = useContext(LoggedInContext);
 
   //   if (!loggedInCTX.isLoggedIn)
-  if (!loggedInCTX.isLoggedIn && !loggedInCTX.isAdmin)
-    return <Navigate to="/store" />;
+  if (!loggedInCTX.isLoggedIn && !loggedInCTX.isAdmin) return <Navigate to="/store" />;
 
   return (
     <div>
       Admin Page
-      <div>
-        Is the user logged in still? {loggedInCTX.isLoggedIn ? "yepp" : "nope"}{" "}
-      </div>
+      <div>Is the user logged in still? {loggedInCTX.isLoggedIn ? "yepp" : "nope"} </div>
       <div>Is the user an admin? {loggedInCTX.isAdmin ? "yepp" : "nope"}</div>
+      <Link to="../editproducts">
+        <button className="">Edit products</button>
+      </Link>
     </div>
   );
 }
