0f2a9decdc4968c3f84c1ec93323fadc905f1908
JustSteve2k <srodriguez2k@gmail.com>
2023-03-22 22:50:20 -0500

    add wakeupService function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { NavLink } from "react-router-dom";
 
 import classes from "./Home.module.css";
@@ -7,10 +8,30 @@ import { useTitleSetter } from "../../Utilities/Utilities";
 import Data from "../../Data";
 
 export default function Home() {
-
   useTitleSetter(Data.homeTitle);
 
   const entryVerbage = Data.homeScreenGreeting;
+
+  const onlineUrl = "https://jealous-puce-vestments.cyclic.app/main";
+
+  const wakeupService = async () => {
+    let info = await fetch(onlineUrl, { method: "GET" })
+      .then((resp) => {
+        if (!resp.ok) {
+          throw Error("There was an issue.");
+        }
+        return resp.json();
+      })
+      .then((data) => data.response);
+
+    return info;
+  };
+
+  const query = useQuery("wakeup", wakeupService);
+
+  if (query.isFetched) {
+    console.log(query.data);
+  }
 
   return (
     <div className={classes.homeBackground}>
